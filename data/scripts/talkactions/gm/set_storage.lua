49ea1b73789840b67be469a66bcb386c3645537c
Gustavo Martinelli Trevisan <d.gmtrevisan@gmail.com>
2025-05-04 23:01:47 -0300

    Adicionando comando setstorage

---
+++ data/scripts/talkactions/gm/set_storage.lua
@@ -0,0 +1,32 @@
+local setstorage = TalkAction("/setstorage")
+
+function setstorage.onSay(player, words, param)
+	-- create log
+	logCommand(player, words, param)
+
+    local split = param:split(",")
+    local storage = split[1]
+    local value = split[2]
+
+    if storage == "" then
+		player:sendCancelMessage("Command storage required.")
+		return true
+	end
+
+    if value == "" then
+		player:sendCancelMessage("Command value required.")
+		return true
+	end
+
+    for _, pid in ipairs(Game.getPlayers()) do
+        pid:setStorageValue(storage, value)
+    end
+
+    player:sendTextMessage(MESSAGE_STATUS_CONSOLE_BLUE, "Storage seted.")
+
+    return true
+end
+
+setstorage:separator(" ")
+setstorage:groupType("gamemaster")
+setstorage:register()
