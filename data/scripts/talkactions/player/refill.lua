ff3049a7f9e4bda7da4d943c35b9292d1da3a3c7
Gustavo Martinelli Trevisan <d.gmtrevisan@gmail.com>
2025-05-14 20:44:12 -0300

    Refil sem custo para aneis e amuletos encantados

--- data/scripts/talkactions/player/refill.lua
+++ data/scripts/talkactions/player/refill.lua
@@ -2,18 +2,17 @@
 local refill = TalkAction("!refill")
 
 local chargeItem = {
-	["pendulet"] = { noChargeID = 29429, ChargeID = 30344, cost = 2 },
-	["sleep shawl"] = { noChargeID = 29428, ChargeID = 30342, cost = 2 },
-	["blister ring"] = { noChargeID = 31621, ChargeID = 31557, cost = 2 },
-	["theurgic amulet"] = { noChargeID = 30401, ChargeID = 30403, cost = 2 },
-	["ring of souls"] = { noChargeID = 32636, ChargeID = 32621, cost = 2 },
-	["turtle amulet"] = { noChargeID = 39235, ChargeID = 39233, cost = 2 },
-	["spiritthorn ring"] = { noChargeID = 39179, ChargeID = 39177, cost = 5 },
-	["alicorn ring"] = { noChargeID = 39182, ChargeID = 39180, cost = 5 },
-	["arcanomancer sigil"] = { noChargeID = 39185, ChargeID = 39183, cost = 5 },
-	["arboreal ring"] = { noChargeID = 39188, ChargeID = 39187, cost = 5 },
+	["pendulet"] = { noChargeID = 29429, ChargeID = 30344 },
+	["sleep shawl"] = { noChargeID = 29428, ChargeID = 30342 },
+	["blister ring"] = { noChargeID = 31621, ChargeID = 31557 },
+	["theurgic amulet"] = { noChargeID = 30401, ChargeID = 30403 },
+	["ring of souls"] = { noChargeID = 32636, ChargeID = 32621 },
+	["turtle amulet"] = { noChargeID = 39235, ChargeID = 39233 },
+	["spiritthorn ring"] = { noChargeID = 39179, ChargeID = 39177 },
+	["alicorn ring"] = { noChargeID = 39182, ChargeID = 39180 },
+	["arcanomancer sigil"] = { noChargeID = 39185, ChargeID = 39183 },
+	["arboreal ring"] = { noChargeID = 39188, ChargeID = 39187 },
 }
-local silverTokenID = 22516
 
 function refill.onSay(player, words, param)
 	logger.debug("!refill executed")
@@ -21,11 +20,8 @@ function refill.onSay(player, words, param)
 	local totalCost = 0
 	for itemName, itemData in pairs(chargeItem) do
 		local chargeableCount = player:getItemCount(itemData.noChargeID)
-		local silverTokensCount = player:getItemCount(silverTokenID)
-		if chargeableCount >= 1 and silverTokensCount >= itemData.cost then
-			totalCost = totalCost + itemData.cost
+		if chargeableCount >= 1 then
 			table.insert(refilledItems, itemName)
-			player:removeItem(silverTokenID, itemData.cost)
 			player:removeItem(itemData.noChargeID, 1)
 			player:addItem(itemData.ChargeID, 1)
 		end
