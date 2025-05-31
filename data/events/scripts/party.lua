5873ff9a0dad7a8aa0b169e090d00bd518375bf0
Gustavo Martinelli Trevisan <d.gmtrevisan@gmail.com>
2025-05-31 10:23:24 -0300

    Alterando formula de share de exp da pt

--- data/events/scripts/party.lua
+++ data/events/scripts/party.lua
@@ -11,33 +11,35 @@ function Party:onDisband()
 end
 
 function Party:onShareExperience(exp)
-    -- local sharedExperienceMultiplier = 1.20 --20%
-    -- local vocationsIds = {}
+    local sharedExperienceMultiplier = 1.30 --30%
+    local vocationsIds = {}
 
-    -- local vocationId = self:getLeader():getVocation():getBase():getId()
-    -- if vocationId ~= VOCATION_NONE then
-    --     table.insert(vocationsIds, vocationId)
-    -- end
+    local vocationId = self:getLeader():getVocation():getBase():getId()
+    if vocationId ~= VOCATION_NONE then
+        table.insert(vocationsIds, vocationId)
+    end
 
-    -- for _, member in ipairs(self:getMembers()) do
-    --     vocationId = member:getVocation():getBase():getId()
-    --     if not table.contains(vocationsIds, vocationId) and vocationId ~= VOCATION_NONE then
-    --         table.insert(vocationsIds, vocationId)
-    --     end
+    for _, member in ipairs(self:getMembers()) do
+        vocationId = member:getVocation():getBase():getId()
+        if not table.contains(vocationsIds, vocationId) and vocationId ~= VOCATION_NONE then
+            table.insert(vocationsIds, vocationId)
+        end
 
-    -- local membersList = self:getMembers()
-    -- local size = #membersList
-    --     sharedExperienceMultiplier = 1.0 + ((size * (5 * (size - 1) + 10)) / 100)
-    -- end
-
-    -- return (exp * sharedExperienceMultiplier) / (#self:getMembers() + 1)
-    local bonusMultiplier = 1.0
     local membersList = self:getMembers()
     local size = #membersList
+        sharedExperienceMultiplier = 1.0 + ((size * (5 * (size - 1) + 10)) / 100)
+    end
 
-    if (size > 1) {
-        bonusMultiplier += 0.2 * (size - 1);
-    }
+    return (exp * sharedExperienceMultiplier) / (#self:getMembers() + 1)
 
-    return (exp * bonusMultiplier)
+
+    -- local bonusMultiplier = 1.0
+    -- local membersList = self:getMembers()
+    -- local size = #membersList
+
+    -- if (size > 1) {
+    --     bonusMultiplier += 0.2 * (size - 1);
+    -- }
+
+    -- return (exp * bonusMultiplier)
 end 
